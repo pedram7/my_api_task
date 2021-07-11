a9e489e41587e29290821060fa852cba556e3102
pedram <pedram7799@yahoo.com>
2021-07-11 21:47:31 +0430

    models init + settings configure

--- core/models.py
+++ core/models.py
@@ -1,3 +1,31 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
 # Create your models here.
+import core
+
+
+class BlogUser(AbstractUser):
+    biography = models.CharField(max_length=255)
+
+    def __str__(self):
+        return "{} {}({})".format(self.first_name, self.last_name, self.username)
+
+
+class Post(models.Model):
+    text = models.TextField()
+    likes = models.ManyToManyField(BlogUser, related_name='liked_posts', blank=True, null=True)
+    dislikes = models.ManyToManyField(BlogUser, related_name='disliked_posts', null=True, blank=True)
+    user = models.ForeignKey(BlogUser, related_name='posts', on_delete=models.CASCADE)
+    posted_time = models.DateTimeField(auto_now_add=True)
+    last_edited = models.DateTimeField(auto_now=True)
+
+
+class Comment(models.Model):
+    parent_comment = models.ForeignKey(core.models.Comment, on_delete=models.CASCADE, null=True, blank=True)
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    text = models.TextField()
+    likes = models.ManyToManyField(BlogUser, related_name='liked_posts', blank=True, null=True)
+    dislikes = models.ManyToManyField(BlogUser, related_name='disliked_posts', blank=True, null=True)
+    user = models.ForeignKey(BlogUser, related_name='comments', on_delete=models.CASCADE)
+    comment_time = models.DateTimeField(auto_now_add=True)
--- my_api_task/settings.py
+++ my_api_task/settings.py
@@ -15,7 +15,6 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
@@ -27,7 +26,6 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
-
 # Application definition
 
 INSTALLED_APPS = [
@@ -38,13 +36,14 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'core.apps.CoreConfig',
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
+    #'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
@@ -69,8 +68,18 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'my_api_task.wsgi.application'
+REST_FRAMEWORK = {
+    'DEFAULT_RENDERER_CLASSES': [
+        'rest_framework.renderers.JSONRenderer',
+        'rest_framework.renderers.TemplateHTMLRenderer',
+    ],
+    'DEFAULT_AUTHENTICATION_CLASSES': [
 
+        'rest_framework.authentication.SessionAuthentication',
+    ],
+}
+
+WSGI_APPLICATION = 'my_api_task.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
@@ -81,7 +90,6 @@ DATABASES = {
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
@@ -101,25 +109,24 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
 # Internationalization
 # https://docs.djangoproject.com/en/3.2/topics/i18n/
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Tehran'
 
 USE_I18N = True
 
 USE_L10N = True
 
-USE_TZ = True
-
+USE_TZ = False
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+AUTH_USER_MODEL = 'core.BlogUser'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
