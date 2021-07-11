4fc2c9bab81d9b475e296c043ddcdee1990272ce
pedram <pedram7799@yahoo.com>
2021-07-12 04:17:34 +0430

    like and dislike, all APIs done

--- core/views.py
+++ core/views.py
@@ -2,6 +2,8 @@ from django.contrib.auth import login, logout
 from django.shortcuts import render
 from rest_framework import views, authentication
 from rest_framework.response import Response
+from django.contrib.auth.decorators import login_required
+from rest_framework.decorators import api_view, authentication_classes
 from rest_framework.serializers import ValidationError
 
 # from .forms import AppointmentForm
@@ -78,7 +80,7 @@ class PostView(views.APIView):
                 else:
                     return Response(status=400)
             except Post.DoesNotExist:
-                return Response({'message': 'no such post'}, status=400)
+                return Response({'message': 'no such post'}, status=404)
             if request.user == post.user:
                 serializer.update(post, serializer.validated_data)
                 return Response({'message': 'edited successfully'}, status=200)
@@ -91,7 +93,7 @@ class PostView(views.APIView):
         try:
             post = Post.objects.get(id=request.data.get('post_id'))
         except:
-            return Response({'message': 'no such post'}, status=400)
+            return Response({'message': 'no such post'}, status=404)
         if post.user == request.user:
             post.delete()
             return Response({'message': 'deleted successfully'}, status=200)
@@ -114,9 +116,83 @@ class CommentView(views.APIView):
         try:
             comment = Comment.objects.get(id=request.data.get('comment_id'))
         except:
-            return Response({'message': 'no such comment'}, status=400)
+            return Response({'message': 'no such comment'}, status=404)
         if comment.user == request.user:
             comment.delete()
             return Response({'message': 'deleted successfully'}, status=200)
         else:
             return Response(status=403)
+
+
+@api_view(['POST'])
+@login_required(login_url='/api/login')
+@authentication_classes([CsrfExemptSessionAuthentication])
+def like_post(request):
+    user = request.user
+    try:
+        post = Post.objects.get(id=request.POST.get('post_id'))
+    except:
+        return Response(status=404)
+    if user in post.dislikes.all():
+        post.dislikes.remove(user)
+    post.likes.add(user)
+    return Response(status=200)
+
+
+@api_view(['POST'])
+@login_required(login_url='/api/login')
+@authentication_classes([CsrfExemptSessionAuthentication])
+def dislike_post(request):
+    user = request.user
+    try:
+        post = Post.objects.get(id=request.POST.get('post_id'))
+    except:
+        return Response(status=404)
+    if user in post.likes.all():
+        post.likes.remove(user)
+    post.dislikes.add(user)
+    return Response(status=200)
+
+
+@api_view(['POST'])
+@login_required(login_url='/api/login')
+@authentication_classes([CsrfExemptSessionAuthentication])
+def like_comment(request):
+    user = request.user
+    try:
+        comment = Comment.objects.get(id=request.POST.get('comment_id'))
+    except:
+        return Response(status=404)
+    if user in comment.dislikes.all():
+        comment.dislikes.remove(user)
+    comment.likes.add(user)
+    return Response(status=200)
+
+
+@api_view(['POST'])
+@login_required(login_url='/api/login')
+@authentication_classes([CsrfExemptSessionAuthentication])
+def dislike_comment(request):
+    user = request.user
+    try:
+        comment = Comment.objects.get(id=request.POST.get('comment_id'))
+    except:
+        return Response(status=404)
+    if user in comment.likes.all():
+        comment.likes.remove(user)
+    comment.dislikes.add(user)
+    return Response(status=200)
+
+
+@api_view(['GET'])
+@authentication_classes([CsrfExemptSessionAuthentication])
+def is_edited(request):
+    try:
+        post = Post.objects.get(id=request.GET.get('post_id'))
+    except:
+        return Response(status=404)
+    message = True
+    if post.last_edited == post.posted_time:
+        message = False
+
+    return Response({'message': message}, status=200)
