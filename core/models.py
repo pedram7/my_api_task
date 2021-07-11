2f3368a46dde5603f5560a13751e691781596904
pedram <pedram7799@yahoo.com>
2021-07-12 01:39:36 +0430

    alter biography required add slash to urls

--- core/models.py
+++ core/models.py
@@ -6,7 +6,7 @@ from django.db import models
 
 
 class BlogUser(AbstractUser):
-    biography = models.CharField(max_length=255)
+    biography = models.CharField(max_length=255, blank=True, null=True)
 
     def __str__(self):
         return "{} {}({})".format(self.first_name, self.last_name, self.username)
